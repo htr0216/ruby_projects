d0e97579c715460ca90604e8b354f7ed293232ea
kobayashihotaru <blue02.sh@icloud.com>
2020-04-05 08:36:32 +0000

    課題11

--- hello.rb
+++ hello.rb
@@ -5,3 +5,28 @@ p "HR Tech"
 puts 1
 puts 2
 puts 3
+
+#課題１１
+
+puts "初めてのruby"
+puts "ruby" + "はじめました"
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+num_array = [1,2,3,4]
+puts num_array
+  hotaru = { name: "小林 蛍", birth: "1993年2月16日", blood: "A型"}
+puts hotaru[:name]
+puts hotaru[:birth]
+puts hotaru[:blood]
+
+def plus_ruby(name)
+  "#{name}ruby"
+end
+puts plus_ruby("imo")
+
+def plus_one(number)
+  number+1
+end
+puts plus_one(4)
